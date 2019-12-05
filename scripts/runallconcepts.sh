aecad608c1fec153011643d59c497f925b4ff89b
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2019-12-05 16:26:53 -0500

    update to scripts

--- scripts/runallconcepts.sh
+++ scripts/runallconcepts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 1 = root bucket for project
+
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'studyids=($(cat ../studyids.txt))'
 
    cp ../jars/GenerateAllConcepts.jar ../../
@@ -62,6 +64,7 @@ rm -rf ../../completed/ConsentGroupVariable.csv
 
 cp ../jars/DbgapGlobalVarGenerator.jar ../../
 cp ../jars/HarmonizedConsentsGenerator.jar ../../
+cp ../jars/DbGapDataMerge.jar ../../
 
 for studyid in ${studyids[@]}; do
    find data/ -type f -exec rm -rf {} \;
@@ -73,13 +76,17 @@ for studyid in ${studyids[@]}; do
 
    aws s3 cp s3://$1/${studyid}/rawData/data/ data/${studyid,,}/ --recursive --exclude "*" --include "*multi*" --include "*Multi*" --include "*MULTI*"
 
-   java -jar DbgapGlobalVarGenerator.jar -propertiesfile resources/${studyid,,}/job.config
+   java -jar ../../DbgapGlobalVarGenerator.jar -propertiesfile resources/${studyid,,}/job.config
    
 done
    
 java -jar ../../HarmonizedConsentsGenerator.jar -propertiesfile resources/${studyid,,}/job.config
 
 aws s3 cp ../../completed/ConsentGroupVariable.csv s3://$1/data/ConsentGroupVariable.csv
+
 aws s3 cp ../../mappings/consentmapping.csv s3://$1/mappings/consentmapping.csv
+
+nohup java -jar ../../DbGapDataMerge.jar -propertiesfile resources/job.config -Xmx50g &
+
 
 
