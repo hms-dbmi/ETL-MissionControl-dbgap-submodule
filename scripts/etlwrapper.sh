cc455fb4439fb993106b9a1ce602d3bca169bdf8
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2020-04-14 14:47:34 -0400

    update wrapper

--- scripts/etlwrapper.sh
+++ scripts/etlwrapper.sh
@@ -48,23 +48,23 @@ for studyid in ${studyids[@]}; do
    rm -rf completed/*
    mkdir data
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/mappings/ mappings/ --recursive --include "mapping.csv"
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/mappings/ mappings/ --recursive --include "mapping.csv" --quiet
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/resources/ resources/ --recursive --include "mapping.csv" --include "job.config"
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/resources/ resources/ --recursive --include "mapping.csv" --include "job.config" --quiet
 
    sed -i "s/skipdataheader=Y/skipdataheader=N/g" resources/job.config
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/rawData/data/ data/ --recursive
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/rawData/data/ data/ --recursive --quiet
 
    java -jar DbgapDecodeFiles.jar
 
    java -jar DbgapTreeBuilder2.jar -dataseperator '\t'
 
-   aws s3 cp mappings/mapping.csv s3://avillach-73-bdcatalyst-etl/${studyid}/currentmapping.csv
+   aws s3 cp mappings/mapping.csv s3://avillach-73-bdcatalyst-etl/${studyid}/currentmapping.csv --quiet
 
-   aws s3 cp resources/job.config s3://avillach-73-bdcatalyst-etl/${studyid}/current.config
+   aws s3 cp resources/job.config s3://avillach-73-bdcatalyst-etl/${studyid}/current.config --quiet
    
-   aws s3 cp completed/ s3://avillach-73-bdcatalyst-etl/${studyid}/data/ --recursive
+   aws s3 cp completed/ s3://avillach-73-bdcatalyst-etl/${studyid}/data/ --recursive --quiet
 
 done
 echo ""
@@ -86,13 +86,17 @@ for studyid in ${studyids[@]}; do
    rm -rf completed/*
    mkdir data
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/currentmapping.csv mappings/mapping.csv
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/currentmapping.csv mappings/mapping.csv --quiet
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/current.config resources/job.config
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/current.config resources/job.config --quiet
 
-   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/data/ data/ --recursive
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/data/ data/ --recursive --quiet
+   
+   #java -jar Partitioner.jar -propertiesfile resources/job.config --quiet
 
    java -jar GenerateAllConcepts.jar -propertiesfile resources/job.config
+
+   aws s3 cp completed/ s3://avillach-73-bdcatalyst-etl/${studyid}/completed/ --recursive
 
 done
 
