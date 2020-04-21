0e0bf4d724c4e8448b19a1116657e8105da1d53b
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2020-04-21 15:36:52 -0400

    new datamerge

---
+++ scripts/run_datamerge.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#####
+#
+# This script generates global variables.
+#
+#####
+
+#### Config files #####
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'studyids=($(cat ./studyids.txt))'
+
+
+#### Global variables
+
+find data/ -type f -exec rm -rf {} \;
+
+find completed/ -type f -exec rm -rf {} \;
+
+find processing/ -type f -exec rm -rf {} \;
+
+find resources/ -type f -exec rm -rf {} \;
+
+find mappings/ -type f -exec rm -rf {} \;
+
+cp ETL-MissionControl-dbgap-submodule/jars/* .
+
+mkdir data
+mkdir completed
+mkdir hierarchies
+mkdir processing
+
+for studyid in ${studyids[@]}; do
+   aws s3 cp s3://avillach-73-bdcatalyst-etl/${studyid}/completed/${studyid^^}_allConcepts.csv data/
+done
