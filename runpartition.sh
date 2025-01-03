b7918bcd257f836f81de95ac0784ae2ab7c95125
sharra44 <sharra_neely@hms.harvard.edu>
2025-01-03 13:32:37 -0500

    Adding scripts from ETLToolSuite-MissionControl to centralize processes

---
+++ runpartition.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+while getopts m:j:c:r: option
+do
+case "${option}"
+in
+        m) memory=${OPTARG};;
+        j) maxjobs=${OPTARG};;
+        c) configfile=${OPTARG};;
+        r) resdir=${OPTARG};;
+esac
+done
+
+echo ${memory}
+echo $maxjobs
+echo $configfile
+echo $resdir
+
+mkdir etl_logs
+
+for filename in ${resdir}${configfile}; do
+    echo $filename
+    nohup java -jar jars/GenerateAllConcepts.jar -propertiesfile $filename -Xmx${memory} >> etl_logs/${configfile}.log &
+
+    while [ $(ps aux | grep GenerateAllConcepts.jar | wc -l) -gt ${maxjobs} ]; do
+        sleep .2
+    done
+done
+
+while [ $(ps aux | grep GenerateAllConcepts.jar | wc -l) -gt 1 ]; do
+
+   sleep .2
+
+done
