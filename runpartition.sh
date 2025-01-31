0965d754d9f2d2ccac47944e98b5129f9845dbdf
sharra44 <sharra_neely@hms.harvard.edu>
2025-01-30 20:10:43 -0500

    update for new concept path system

--- runpartition.sh
+++ runpartition.sh
@@ -19,7 +19,7 @@ echo $resdir
 mkdir etl_logs
 
 for filename in ${resdir}${configfile}; do
-    echo $filename
+    echo $filename started
     nohup java -jar jars/GenerateAllConcepts.jar -propertiesfile $filename -Xmx${memory} >> etl_logs/${configfile}.log &
 
     while [ $(ps aux | grep GenerateAllConcepts.jar | wc -l) -gt ${maxjobs} ]; do
@@ -32,3 +32,4 @@ while [ $(ps aux | grep GenerateAllConcepts.jar | wc -l) -gt 1 ]; do
    sleep .2
 
 done
+echo all GenerateAllConcepts threads completed
